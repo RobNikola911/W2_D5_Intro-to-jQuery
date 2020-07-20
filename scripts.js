741c2e323fc9c025a5c1192d88042fda667acfdc
RobNikola911 <pajic1991@gmail.com>
2020-07-20 15:16:51 +0200

    finished

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (
+    number > 100
+) {
+   console.log("whoah, that's a big number.") 
+} else {
+    console.log("just a regular number, please.")
+}
+
+/* let third = document.getElementById('third');
+if (word === "cool") {
+    Change the text to Power of DOM of the third
+} else {
+    otherwise change the fourth one
+} */
+
+if (
+    word === "cool"
+) {
+   console.log("Change the text to Power of DOM of the third") 
+} else {
+    console.log("change the fourth one.")
+}
